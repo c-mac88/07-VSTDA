5b0630c9b9b00e2ea35bde94bd7eabb712e6c546
Christian McFarland <Christian McFarland>
2016-04-28 21:42:10 -0700

    final commit

--- index.js
+++ index.js
@@ -2,13 +2,56 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('MainCtrl', function($scope) {
 	$scope.todos = [];
-    $scope.text = '';
-    $scope.submit = function() {
+	$scope.todosH = [];$scope
+    $scope.textH = '';
+    $scope.high = function() {
         if ($scope.text) {
-          $scope.todos.push(this.text);
-          $scope.text = '';
+        	$scope.todos.push(this.text);
+          	$scope.todosH.push(this.text);
+          	$scope.text = '';
         }
-     }
+    }
+    $scope.todosM = [];
+    $scope.text = '';
+	$scope.medium = function() {
+        if ($scope.text) {
+        	$scope.todos.push(this.text);
+          	$scope.todosM.push(this.text);
+          	$scope.text = '';
+        }
+    }
+    $scope.todosL = [];
+    $scope.text = '';
+    $scope.low = function() {
+        if ($scope.text) {
+        	$scope.todos.push(this.text);
+          	$scope.todosL.push(this.text);
+          	$scope.text = '';
+        }
+    }
+
+    $scope.alphabet = function() {
+    	$scope.alph = true;
+		$scope.beta = false; 
+    	$scope.charlie = false; 
+    }
+
+    $scope.betabet = function() {
+    	$scope.alph = false;
+    	$scope.beta = true; 
+    	$scope.charlie = false;  
+    }
+
+    $scope.charliebet = function() {
+    	$scope.alph = false;
+    	$scope.beta = false; 
+    	$scope.charlie = true; 
+    }
+
+
+
+
+
 });
 
 
